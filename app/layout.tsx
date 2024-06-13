099d6b08242311505062449630c346fd604d8eee
조성원 <tjddnjsjo@kakao.com>
2024-06-13 20:46:19 +0900

    refactor: change project folder structure

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next'
 import { GeistSans } from 'geist/font/sans'
 import { GeistMono } from 'geist/font/mono'
-import './globals.css'
+import '@/styles/globals.css'
 
 export const metadata: Metadata = {
   title: 'Tech Orbit',
