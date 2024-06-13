61a8b27166676ffafe836ed93081baca82a2337a
조성원 <tjddnjsjo@kakao.com>
2024-06-14 04:40:10 +0900

    feat: create get api

---
+++ app/api/route.ts
@@ -0,0 +1,24 @@
+import { NextRequest, NextResponse } from 'next/server'
+
+import { TechOrbitSvg } from '@/assets/tech-orbit-svg'
+
+export async function GET(req: NextRequest) {
+  const { searchParams } = req.nextUrl
+  const size = searchParams.get('size') || '1000'
+  const duration = searchParams.get('duration') || '12'
+  const text = searchParams.get('text') || ''
+  const tech = searchParams.get('tech') || ''
+
+  const svg = TechOrbitSvg(
+    Number(size),
+    Number(duration),
+    text,
+    tech.split(','),
+  )
+
+  return new NextResponse(svg, {
+    headers: {
+      'Content-Type': 'image/svg+xml',
+    },
+  })
+}
