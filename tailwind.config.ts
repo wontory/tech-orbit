84bfe3fdd12352ffa40c0ceb140805a9dfc3189d
조성원 <tjddnjsjo@kakao.com>
2024-06-13 20:42:09 +0900

    refactor: remove unnecessary assets & codes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,11 +12,6 @@ const config: Config = {
         sans: ['var(--font-geist-sans)'],
         mono: ['var(--font-geist-mono)'],
       },
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
     },
   },
   plugins: [],
