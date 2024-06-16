f67d2133c6093ee5b064802a2eb34aeb60b39dea
조성원 <tjddnjsjo@kakao.com>
2024-06-16 17:33:18 +0900

    refactor: optimize icons generation and export
    The script `generate-icons.mjs` has been refactored to optimize the generation and export of icons data. The `simple-icons` library is used to create an object containing the SVG and hex values for each icon. The generated data is then written to `icons.ts` in the `src/assets` directory. This commit improves the efficiency of the icons generation process.

--- script/generate-icons.mjs
+++ script/generate-icons.mjs
@@ -12,17 +12,11 @@ Object.keys(simpleIcons).forEach((key) => {
   }
 })
 
-const iconsJson = JSON.stringify(iconsObject, null, 2)
+const iconsJson = JSON.stringify(iconsObject)
 
 fs.writeFileSync(
   './src/assets/icons.ts',
-  `
-type IconsKeyType = keyof typeof icons
-
-const icons = ${iconsJson}
-
-export { type IconsKeyType, icons }
-`,
+  `type IconsKeyType = keyof typeof icons; const icons = ${iconsJson}; export { type IconsKeyType, icons };`,
 )
 
 console.log('Icons data has been generated and saved to iconsData.json')
