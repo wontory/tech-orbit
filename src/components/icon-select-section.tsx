be5b32c3223357c6dc1df4218837f94024b71b36
조성원 <tjddnjsjo@kakao.com>
2024-06-18 03:16:55 +0900

    feat: add scroll-area into icon-select-section

--- src/components/icon-select-section.tsx
+++ src/components/icon-select-section.tsx
@@ -5,6 +5,7 @@ import { useAtom } from 'jotai'
 
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
+import { ScrollArea } from '@/components/ui/scroll-area'
 import { selectedIconsAtom } from '@/atoms/selected-icons'
 import { icons } from '@/assets/icons'
 
@@ -32,26 +33,31 @@ function IconSelectSection() {
     <section className="flex w-full flex-col gap-4">
       <h2 className="text-2xl font-bold">Icons</h2>
       <Input placeholder="Search Icons" onChange={handleSearch} />
-      <div className="grid grid-cols-2 gap-4 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5">
-        {filteredIcons.map((icon, index) => {
-          const svg = icon[1].svg.replace('<svg', `<svg fill="#${icon[1].hex}"`)
+      <ScrollArea className="flex max-h-[calc(100vh-368px)] flex-col">
+        <div className="grid grid-cols-2 gap-4 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5">
+          {filteredIcons.map((icon, index) => {
+            const svg = icon[1].svg.replace(
+              '<svg',
+              `<svg fill="#${icon[1].hex}"`,
+            )
 
-          return (
-            <Button
-              key={`icon-${index}`}
-              variant="outline"
-              className="flex aspect-square h-full w-full flex-col gap-4"
-              onClick={() => handleClick(icon[1].title)}
-            >
-              <div
-                dangerouslySetInnerHTML={{ __html: svg }}
-                className="h-16 w-16"
-              />
-              <span className="text-xs">{icon[1].title}</span>
-            </Button>
-          )
-        })}
-      </div>
+            return (
+              <Button
+                key={`icon-${index}`}
+                variant="outline"
+                className="flex aspect-square h-full w-full flex-col gap-4"
+                onClick={() => handleClick(icon[1].title)}
+              >
+                <div
+                  dangerouslySetInnerHTML={{ __html: svg }}
+                  className="h-16 w-16"
+                />
+                <span className="text-xs">{icon[1].title}</span>
+              </Button>
+            )
+          })}
+        </div>
+      </ScrollArea>
     </section>
   )
 }
