bb9dfb7e80ce68ce4a30358bfeec5717b1bf17b0
조성원 <tjddnjsjo@kakao.com>
2024-06-20 20:24:55 +0900

    refactor: rename icon-button component to icon-label-button

--- src/components/icon-label-button.tsx
+++ src/components/icon-label-button.tsx
@@ -1,13 +1,12 @@
 'use client'
 
-import { memo } from 'react'
 import { useSetAtom } from 'jotai'
 
 import { Button } from '@/components/ui/button'
 import { selectedIconsAtom } from '@/atoms/selected-icons'
 import type { IconType } from '@/types/icons'
 
-const IconButton = memo(({ icon }: { icon: IconType }) => {
+function IconLabelButton({ icon }: { icon: IconType }) {
   const setSelectedIcons = useSetAtom(selectedIconsAtom)
 
   const handleClick = () => setSelectedIcons((prev) => [...prev, icon.title])
@@ -27,8 +26,6 @@ const IconButton = memo(({ icon }: { icon: IconType }) => {
       <span className="text-xs">{icon.title}</span>
     </Button>
   )
-})
+}
 
-IconButton.displayName = 'IconButton'
-
-export { IconButton }
+export { IconLabelButton }
